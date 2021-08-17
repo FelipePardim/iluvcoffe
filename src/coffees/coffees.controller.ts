0e7623d9fed195df36fd999236e3b494987907bf
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2021-08-17 17:38:52 -0300

    Update and delete methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -15,5 +15,15 @@ export class CoffeesController {
     @Post()
     create(@Body() body) {
         return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `This action updates #${id} coffee`;
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `This action removes #${id} coffee`;
     }
 }
