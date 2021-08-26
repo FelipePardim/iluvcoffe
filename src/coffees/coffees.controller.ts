e7d8f90079f2d7204a5eced44238260fbd7d2a40
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2021-08-26 17:46:16 -0300

    Pagination implementation

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -8,9 +9,9 @@ export class CoffeesController {
     constructor(private readonly coffeeService: CoffeesService) {}
 
     @Get()
-    findAll(@Query() paginationQuery) {
+    findAll(@Query() paginationQuery: PaginationQueryDto) {
         //const { limit, offset } = paginationQuery;
-        return this.coffeeService.findAll();
+        return this.coffeeService.findAll(paginationQuery);
     }
 
     @Get(':id')
