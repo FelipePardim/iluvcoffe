366da7159e8648863337c2ba0efd856e6f7ed078
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2021-08-27 15:10:43 -0300

    Coffee recommendation

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Event } from 'src/events/entities/event.entity';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
 @Module({ 
-    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeesController], 
     providers: [CoffeesService],
 })
